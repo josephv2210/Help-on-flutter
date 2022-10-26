bcec6d1dcaae069a74b5c24002c71c563ad6fd2e
josephv_2210 <joseph.2210@hotmail.es>
2022-10-26 10:06:10 -0500

    tabs and pages

---
+++ lib/pages/PageSearch.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PageSearch extends StatelessWidget {
+  const PageSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Search", style: TextStyle(fontSize: 30),),
+    );
+  }
+}
